d2bd47a5a0162df4ae967df82f8d7838247a750e
Enceladus-py <dalsunaberat@gmail.com>
2023-02-21 13:42:05 +0300

    fixed last query

--- hmw4/hmw4.sql
+++ hmw4/hmw4.sql
@@ -6,4 +6,4 @@ SELECT COUNT(*) FROM film WHERE title LIKE 'T%' AND rating = 'G';
 
 SELECT COUNT(*) FROM country WHERE country LIKE '_____';
 
-SELECT COUNT(*) FROM city WHERE city LIKE 'A%a';
+SELECT COUNT(*) FROM city WHERE city ILIKE '%r';
